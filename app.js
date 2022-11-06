0ef1a952b0ccb33458004461959be105bd40750c
unknown <jonathanadrino@gmail.com>
2022-11-06 18:02:03 +0800

    railway deployment 4

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/', (req,res) => {
+  res.status(200).json({
+    message: 'app is running'
+  })
+})
+
 app.get("/user", async (req, res) => {
   try {
     const data = await User.findAll();
